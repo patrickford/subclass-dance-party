6b12a40326602118beb6f8fbf39addc80391b8e4
Hack Reactor Students <patrickford@msn.com>
2014-01-13 18:12:12 -0800

    Added pulsingDancer

---
+++ src/pulsingDancer.js
@@ -0,0 +1,8 @@
+var PulsingDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+}
+
+
+PulsingDancer.prototype.sizeChange = function(){
+  setInterval()
+}
