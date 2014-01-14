aac46c527619bc1244493b707407c225ee2965e4
Walid Hosseini <caasjj@gmail.com>
2014-01-13 23:23:31 -0800

    pbj danceparty

--- src/pbjDancer.js
+++ src/pbjDancer.js
@@ -1,11 +1,28 @@
+var bananaCounter = 0;
+
 var PeanutButterJellyDancer = function(top, left, timeBetweenSteps){
+  bananaCounter++;
   Dancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<span class="pbj">');
+  this.$node.addClass('pbj');
   console.log(this.$node, this, top, left, timeBetweenSteps);
   this.step();
+  // if(bananaCounter > 2){
+  //   this.rotate({
+  //   angle : 0, 
+  //   animateTo : 360, 
+  //   callback : rotation,
+  //   easing : function (x,t,b,c,d){ 
+  //       return c*(t/d)+b; }
+  //  });
+  // }
 };
 
 PeanutButterJellyDancer.prototype = Object.create(Dancer.prototype);
 PeanutButterJellyDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
+};
+
+
+PeanutButterJellyDancer.prototype.rotate = function(){
+  
 };
